ef4e2bbecfabfc7aaa0511ba1295059c7550908a
Lucas Marcolongo <lucas_marco@live.com>
2024-04-19 02:49:39 -0700

    feat(marcolongo.cloud): `webviewer` checkpoint (#66)
    * fix(ci): fix `version` ci by `--parallel=1`
    
    * feat(marcolongo.cloud): `webviewer` checkpoint

--- libs/common-ui/src/lib/components/webviewer/webviewer.component.ts
+++ libs/common-ui/src/lib/components/webviewer/webviewer.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
-  EventEmitter,
   ViewChild,
   ElementRef,
   AfterViewInit,
-  OnInit,
-  Output,
+  Input,
+  OnDestroy,
 } from '@angular/core';
 import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject, delay, takeUntil, tap } from 'rxjs';
+
+// TODO: CORS proxy is a workaround for the WebDav server not supporting CORS
+const BASE_URL = `https://corsproxy.io/?${encodeURIComponent('https://u396144.your-storagebox.de')}`;
+const USERNAME = 'u396144';
+const PASSWORD = 'HYqbjPFcsvRePJ7s';
+const TOKEN = btoa(`${USERNAME}:${PASSWORD}`);
 
 @Component({
   selector: 'lib-webviewer',
@@ -18,25 +23,50 @@ import { Subject } from 'rxjs';
   templateUrl: './webviewer.component.html',
   styleUrl: './webviewer.component.scss',
 })
-export class WebViewerComponent implements OnInit, AfterViewInit {
-  wvInstance?: WebViewerInstance;
-
+export class WebViewerComponent implements AfterViewInit, OnDestroy {
   @ViewChild('viewer', {
     static: true,
   })
   viewer!: ElementRef;
 
-  @Output() coreControlsEvent: EventEmitter<string>;
+  wvInstance?: WebViewerInstance;
 
-  private documentLoaded$: Subject<void>;
+  #documentLoaded$: Subject<void>;
+
+  #file$: BehaviorSubject<string>;
+
+  #ngUnsubscribe$ = new Subject<void>();
 
   constructor() {
-    this.coreControlsEvent = new EventEmitter<string>();
-    this.documentLoaded$ = new Subject<void>();
+    this.#documentLoaded$ = new Subject<void>();
+    this.#file$ = new BehaviorSubject<string>('');
+    // SECTION: Subscribe to file changes
+    // eslint-disable-next-line rxjs-angular/prefer-async-pipe, rxjs-angular/prefer-composition
+    this.#file$
+      .pipe(
+        delay(1000), // ! This is a workaround for the WebViewer not being ready when the file is set
+        // TODO: Add a loading spinner and handle the loading state properly
+        tap((file) => {
+          console.log('🚀 ~ WebViewerComponent ~ tap ~ fileUrl:', file);
+          if (this.wvInstance) {
+            const fileUrl = `${BASE_URL}/${file}.pdf`;
+            this.wvInstance.UI.loadDocument(fileUrl, {
+              customHeaders: {
+                // TODO: We will need to handle user authentication towards WebDav
+                Authorization: `Basic ${TOKEN}`,
+              },
+            });
+          }
+        }),
+        takeUntil(this.#ngUnsubscribe$),
+      )
+      // eslint-disable-next-line rxjs-angular/prefer-async-pipe, rxjs-angular/prefer-composition, rxjs-angular/prefer-takeuntil
+      .subscribe();
   }
 
-  ngOnInit(): void {
-    console.log('🚀 ~ PdfEditorComponent ~ ngOnInit ~ ngOnInit:', 'ngOnInit');
+  @Input() set file(value: string) {
+    this.#file$.next(value);
+    console.log('🚀 ~ WebViewerComponent ~ @Input ~ value:', value);
   }
 
   ngAfterViewInit(): void {
@@ -45,13 +75,21 @@ export class WebViewerComponent implements OnInit, AfterViewInit {
         path: 'assets/webviewer',
         licenseKey:
           'demo:1713206501243:7f12ce5803000000008ffbc3ee0c80a6513940eee53be3ae6c0c231ec6',
-        initialDoc:
-          'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
+        fullAPI: true,
       },
       this.viewer.nativeElement,
     ).then((instance: WebViewerInstance) => {
       this.wvInstance = instance;
-      this.documentLoaded$.next();
+      this.wvInstance.UI.enableElements([
+        'bookmarksPanel',
+        'bookmarksPanelButton',
+        'outlinesPanel',
+      ]);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.#ngUnsubscribe$.next();
+    this.#ngUnsubscribe$.complete();
   }
 }
