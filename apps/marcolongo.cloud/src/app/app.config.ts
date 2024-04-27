aaac2fac2d66c5a353d7849507f44c1ba65384a5
Lucas Marcolongo <lucas_marco@live.com>
2024-04-26 20:48:43 -0700

    chore: initial commit

---
+++ apps/marcolongo.cloud/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withComponentInputBinding,
+} from '@angular/router';
+
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+      appRoutes,
+      withEnabledBlockingInitialNavigation(),
+      withComponentInputBinding(),
+    ),
+  ],
+};
