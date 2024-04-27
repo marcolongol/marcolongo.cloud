16204c894052e2685e968e93b67705f3fb8d2d4d
Lucas Marcolongo <lucas_marco@live.com>
2024-04-27 01:39:39 -0700

    feat(marcolongo.cloud): add support for `environment` files

---
+++ apps/marcolongo.cloud/src/environments/environment.production.ts
@@ -0,0 +1,18 @@
+import { baseEnvironment } from './environment.base';
+import { DeepReadOnly } from '../shared/types/types';
+
+/**
+ * The production environment configuration.
+ * The environment is protected by a read-only mechanism.
+ * This means that the environment cannot be modified at runtime.
+ * @example
+ * // This will throw an error
+ * environment.production = false;
+ * @example
+ * // This will throw an error
+ * environment.apiParams.baseUrl = 'https://api.marcolongo.cloud';
+ */
+export const environment: DeepReadOnly<typeof baseEnvironment> = {
+  ...baseEnvironment,
+  production: true,
+};
