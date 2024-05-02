a58f591f105d8ba3d110e3e3653e5a2930a05cde
Lucas Marcolongo <lucas_marco@live.com>
2024-05-02 11:31:22 -0700

    chore: tweaking storybook params

--- libs/common-ui/.storybook/main.ts
+++ libs/common-ui/.storybook/main.ts
@@ -3,6 +3,12 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  staticDirs: [
+    {
+      from: '../src/assets',
+      to: 'assets',
+    },
+  ],
   framework: {
     name: '@storybook/angular',
     options: {},
--- libs/common-ui/.storybook/preview.ts
+++ libs/common-ui/.storybook/preview.ts
@@ -1,1 +1,10 @@
 /* eslint-disable unicorn/no-empty-file */
+import { Preview } from '@storybook/angular';
+
+const preview: Preview = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default preview;
