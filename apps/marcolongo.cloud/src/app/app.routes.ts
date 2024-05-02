d93aa41de0e0ca075c4589cf28cf0c17251aa5d4
Lucas Marcolongo <lucas_marco@live.com>
2024-05-01 22:50:21 -0700

    develop (#103)
    * chore: update prettier and editorconfig parameters
    
    * fixup! chore: update prettier and editorconfig parameters
    
    * fixup! fixup! chore: update prettier and editorconfig parameters
    
    * chore: apply formatting

--- apps/marcolongo.cloud/src/app/app.routes.ts
+++ apps/marcolongo.cloud/src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'home',
-    loadComponent: () =>
-      import('./home/home.component').then((m) => m.HomeComponent),
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'webviewer',
--- apps/marcolongo.cloud/src/environments/environment.development.ts
+++ apps/marcolongo.cloud/src/environments/environment.development.ts
@@ -2,15 +2,16 @@ import { baseEnvironment } from './environment.base';
 import { DeepReadOnly } from '../shared/types/types';
 
 /**
- * The development environment configuration.
- * The environment is protected by a read-only mechanism.
- * This means that the environment cannot be modified at runtime.
+ * The development environment configuration. The environment is protected by a
+ * read-only mechanism. This means that the environment cannot be modified at runtime.
+ *
  * @example
- * // This will throw an error
- * environment.production = false;
+ *   // This will throw an error
+ *   environment.production = false;
+ *
  * @example
- * // This will throw an error
- * environment.apiParams.baseUrl = 'https://api.marcolongo.cloud';
+ *   // This will throw an error
+ *   environment.apiParams.baseUrl = 'https://api.marcolongo.cloud';
  */
 export const environment: DeepReadOnly<typeof baseEnvironment> = {
   ...baseEnvironment,
