2c263d8feb3565942ae3904b028f0785618a1680
Lucas Marcolongo <lucas_marco@live.com>
2024-05-02 19:49:35 -0700

    chore: tweaking storybook params (#109)

--- libs/common-ui/src/lib/components/webviewer/webviewer.component.stories.ts
+++ libs/common-ui/src/lib/components/webviewer/webviewer.component.stories.ts
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { expect } from '@storybook/jest';
-import { within } from '@storybook/testing-library';
 
 import { WebViewerComponent } from './webviewer.component';
 
@@ -11,18 +9,4 @@ const meta: Meta<WebViewerComponent> = {
 export default meta;
 type Story = StoryObj<WebViewerComponent>;
 
-export const Primary: Story = {
-  args: {
-    file: '',
-  },
-};
-
-export const Heading: Story = {
-  args: {
-    file: '',
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/webviewer works!/gi)).toBeTruthy();
-  },
-};
+export const Primary: Story = {};
