8e580c41acf20a5402d7454c25efeda46075a50a
Lucas Marcolongo <lucas_marco@live.com>
2024-05-04 14:01:57 -0700

    chore: tweaking storybook params pt.2

--- libs/common-ui/src/lib/components/webviewer/webviewer.component.stories.ts
+++ libs/common-ui/src/lib/components/webviewer/webviewer.component.stories.ts
@@ -9,4 +9,59 @@ const meta: Meta<WebViewerComponent> = {
 export default meta;
 type Story = StoryObj<WebViewerComponent>;
 
-export const Primary: Story = {};
+export const Fullscreen: Story = {
+  decorators: [
+    () => ({
+      template: `
+      <div style="height: 100vh; width: 100vw;">
+        <lib-webviewer></lib-webviewer>
+      </div>
+      `,
+    }),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Container: Story = {
+  decorators: [
+    () => ({
+      styles: [
+        `
+        .container {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          flex-direction: row;
+        }
+
+        .left-col {
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+        }
+
+        .right-col {
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          height: 800px;
+        }
+
+      `,
+      ],
+      template: `
+      <div class="container">
+        <div class="left-col">
+          <h1>Left Column</h1>
+        </div>
+        <div class="right-col">
+          <h1>Right Column</h1>
+          <lib-webviewer></lib-webviewer>
+        </div>
+      </div>
+      `,
+    }),
+  ],
+};
