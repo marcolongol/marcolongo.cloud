cc3910661434923275a93656e0e1546fa5be32a9
Lucas Marcolongo <lucas_marco@live.com>
2024-05-08 22:35:33 -0700

    feat(common-ui): add `ThemeSelectorComponent` to `HeaderComponent` (#137)

--- libs/common-ui/src/lib/components/theme-selector/theme-selector.component.ts
+++ libs/common-ui/src/lib/components/theme-selector/theme-selector.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 
 import { Theme, Themes } from '../../../shared/types';
 import { ThemeService } from '../../services/theme/theme.service';
@@ -16,12 +16,15 @@ export class ThemeSelectorComponent implements OnInit {
   public themeService = inject(ThemeService);
   public themes = Themes;
 
+  @Output() themeChange = new EventEmitter<Theme>();
+
   ngOnInit(): void {
     console.log('ThemeSelectorComponent initialized');
     console.log('Current theme:', this.themeService.getTheme());
   }
 
-  selectTheme(theme: Theme): void {
+  selectTheme(event: Event): void {
+    const theme = (event.target as HTMLSelectElement).value as Theme;
     this.themeService.setTheme(theme);
   }
 }
