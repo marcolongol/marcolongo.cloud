2c80b8d51b699f89cd84a75a66ee506577291988
Lucas Marcolongo <lucas_marco@live.com>
2024-05-11 01:24:53 -0700

    chore(common-ui): refactor `APP_ROUTES` to use signals

--- libs/core/utils/src/lib/helpers.ts
+++ libs/core/utils/src/lib/helpers.ts
@@ -1,19 +1,41 @@
-import { InjectionToken, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { InjectionToken, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Router, NavigationEnd } from '@angular/router';
+import { map, filter, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { NavItem } from './types';
-export const appRoutesFactory = (): NavItem[] => {
+export const appRoutesFactory = (): Signal<NavItem[]> => {
   const router = inject(Router);
-  return router.config
-    .filter((route) => route.data?.['label'])
-    .map((route) => ({
-      path: route.path,
-      label: route.data?.['label'] || route.path,
-      icon: route.data?.['icon'] || 'link',
-    }));
+
+  const routes$ = router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map(() => {
+      return router.config
+        .filter((route) => route.data?.['label'])
+        .map((route) => {
+          const navItem: NavItem = {
+            path: String(route.path),
+            label:
+              route.data && route.data['label'] ? String(route.data['label']) : '',
+            icon:
+              route.data && route.data['icon'] ? String(route.data['icon']) : 'link',
+          };
+          return navItem;
+        });
+    }),
+    catchError((error) => {
+      console.error('Error in appRoutesFactory', error);
+      return of([]);
+    }),
+  );
+
+  return toSignal(routes$, {
+    initialValue: [],
+  });
 };
 
-export const APP_ROUTES = new InjectionToken<NavItem[]>('APP_ROUTES', {
+export const APP_ROUTES = new InjectionToken<Signal<NavItem[]>>('APP_ROUTES', {
   providedIn: 'root',
   factory: () => appRoutesFactory(),
 });
