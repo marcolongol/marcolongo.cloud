1d017f43f685f1c2ac3d68baaf199cc3b72ee021
Lucas Marcolongo <lucas_marco@live.com>
2024-05-11 00:06:29 -0700

    feat(marcolongo.cloud): add route titles (#144)

--- apps/marcolongo.cloud/src/app/app.routes.ts
+++ apps/marcolongo.cloud/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
+    title: 'Home',
     data: { label: 'Home', icon: 'home' },
     loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'webviewer',
+    title: 'WebViewer',
     data: { label: 'WebViewer', icon: 'web' },
     loadComponent: () =>
       import('@marcolongo.cloud/common-ui/webviewer').then(
@@ -16,17 +18,20 @@ export const appRoutes: Route[] = [
   },
   {
     path: 'gOS',
+    title: 'Gradient OS',
     data: { label: 'Gradient OS', icon: 'gradient' },
     loadChildren: () =>
       import('@marcolongo.cloud/gradient-os').then((m) => m.gradientRoutes),
   },
   {
     path: 'about',
+    title: 'About',
     data: { label: 'About', icon: 'info' },
     redirectTo: '',
   },
   {
     path: '**',
+    title: 'Not Found',
     redirectTo: '',
   },
 ];
