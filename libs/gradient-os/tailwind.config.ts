96f1d92517d3404f473c2a39c0df3a3969c839ae
Lucas Marcolongo <lucas_marco@live.com>
2024-05-14 19:29:21 -0700

    fix: tailwind references for libraries

--- libs/gradient-os/tailwind.config.ts
+++ libs/gradient-os/tailwind.config.ts
@@ -6,7 +6,7 @@ import { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    nodePath.join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    nodePath.join(__dirname, '**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
