716890531ddaed0ac9b4c0a8440878df4c1b0508
Lucas Marcolongo <lucas_marco@live.com>
2024-05-15 18:36:57 -0700

    fix(marcolongo.cloud): tests

--- apps/marcolongo.cloud/src/app/components/sqli/sqli.component.ts
+++ apps/marcolongo.cloud/src/app/components/sqli/sqli.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit, inject } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sqli',
@@ -9,11 +9,4 @@ import { Component, OnInit, inject } from '@angular/core';
   templateUrl: './sqli.component.html',
   styleUrl: './sqli.component.scss',
 })
-export class SqliComponent implements OnInit {
-  private http = inject(HttpClient);
-  ngOnInit(): void {
-    this.http.get('/api').subscribe((data) => {
-      console.log(data);
-    });
-  }
-}
+export class SqliComponent {}
