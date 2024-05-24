b560f2974600a71180e7ab26a542a0d9547cb560
Lucas Marcolongo <lucas_marco@live.com>
2024-05-24 01:57:59 -0700

    feat(marcolongo.cloud-api): add swagger docs

--- apps/marcolongo.cloud-api/src/main.ts
+++ apps/marcolongo.cloud-api/src/main.ts
@@ -2,6 +2,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -10,6 +11,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Marcolongo Cloud API')
+    .setDescription('API for Marcolongo Cloud')
+    .setVersion('1.0')
+    .addTag('marcolongo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
