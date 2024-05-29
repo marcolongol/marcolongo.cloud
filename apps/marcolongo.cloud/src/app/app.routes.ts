b7f12bbeb20c4a6336d10553b967f53b2efbdc7a
Lucas Marcolongo <lucas_marco@live.com>
2024-05-29 11:37:02 -0700

    chore(marcolongo.cloud): route updates

--- apps/marcolongo.cloud/src/app/app.routes.ts
+++ apps/marcolongo.cloud/src/app/app.routes.ts
@@ -9,28 +9,6 @@ export const appRoutes: Route[] = [
       import('./components/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'webviewer',
-    title: 'WebViewer',
-    data: { label: 'WebViewer', icon: 'fa-eye' },
-    loadComponent: () =>
-      import('@marcolongo.cloud/common-ui/webviewer').then(
-        (m) => m.WebViewerComponent,
-      ),
-  },
-  {
-    path: 'gOS',
-    title: 'Gradient OS',
-    data: { label: 'Gradient OS', icon: 'fa-desktop' },
-    loadChildren: () =>
-      import('@marcolongo.cloud/gradient-os').then((m) => m.gradientRoutes),
-  },
-  {
-    path: 'about',
-    title: 'About',
-    data: { label: 'About', icon: 'fa-info-circle' },
-    redirectTo: 'about',
-  },
-  {
     path: '**',
     title: 'Not Found',
     redirectTo: 'not-found',
